4861a36d8948d3a3740bff3b429576c2fcbada2e
oksana_erm <oks.erm.012@gmail.com>
2023-02-11 19:35:43 +0000

    setup basic server(this time for real)

---
+++ backend/server.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: '124',
+      name: 'Sally',
+      email: 'sally@gmail.com',
+      password: 'bananas',
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+}
+
+app.get('/', (req, res) => {
+  res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  if (username === database.users[0].username && password === database.users[0].password) {
+    res.json('success');
+  }
+  else {
+    res.status(400).json('error logging in, you idiot');
+  }
+});
+
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: '125',
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date()
+  })
+  res.json(database.users[database.users.length - 1]);
+})
+
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  })
+  if (!found) {
+    res.status(400).json('not found');
+  }
+})
+
+app.post('/image', (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  })
+  if (!found) {
+    res.status(400).json('not found');
+  }
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
